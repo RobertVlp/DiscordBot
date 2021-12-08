85210e085b1aa4ef7dcd0c528a24782a3f0ca3fc
Student USO VM User <student@stud.acs.upb.ro>
2021-12-08 16:45:26 +0200

    Added license
    Signed-off-by: Student USO VM User <student@stud.acs.upb.ro>

--- Bot/main.py
+++ Bot/main.py
@@ -1,4 +1,4 @@
-#    This file is part of DiscrodBot.
+#    This file is part of DiscordBot.
 
 #    DiscordBot is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
